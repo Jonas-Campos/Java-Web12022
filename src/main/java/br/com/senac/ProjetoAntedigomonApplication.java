3087552a9ba6e88e1f79ac88c33c5bdae81ed781
ADS 2021.1N <36124872021.1N@senacrjedu.local>
2022-08-11 21:40:05 -0300

    SEU BOSTA

---
+++ src/main/java/br/com/senac/ProjetoAntedigomonApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAntedigomonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAntedigomonApplication.class, args);
+	}
+
+}
